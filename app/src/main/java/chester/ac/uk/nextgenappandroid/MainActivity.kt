490f6809050b75ee78f6094b85523c1548ab568e
1721102 <1721102@chester.ac.uk>
2019-05-20 13:01:05 +0100

    Started project added toolbar and navbar

--- app/src/main/java/chester/ac/uk/nextgenappandroid/MainActivity.kt
+++ app/src/main/java/chester/ac/uk/nextgenappandroid/MainActivity.kt
@@ -2,11 +2,22 @@ package chester.ac.uk.nextgenappandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigation)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
