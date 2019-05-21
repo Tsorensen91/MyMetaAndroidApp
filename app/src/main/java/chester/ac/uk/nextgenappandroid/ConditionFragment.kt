867c7b7fe6cff18dbe940c62b6cdafec2d04d7c2
1721102 <1721102@chester.ac.uk>
2019-05-21 13:00:08 +0100

    implemented fragments with navbar

---
+++ app/src/main/java/chester/ac/uk/nextgenappandroid/ConditionFragment.kt
@@ -0,0 +1,21 @@
+package chester.ac.uk.nextgenappandroid
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class ConditionFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_condition, container, false)
+    }
+
+
+}
--- app/src/main/java/chester/ac/uk/nextgenappandroid/MainActivity.kt
+++ app/src/main/java/chester/ac/uk/nextgenappandroid/MainActivity.kt
@@ -3,21 +3,67 @@ package chester.ac.uk.nextgenappandroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val fragment1 = TransitionTrackerFragment()
+    val fragment2 = ConditionFragment()
+    val fragment3 = CalendarFragment()
+    val fragment4 = MailtrackerFragment()
+    val fragment5 = DiettrackerFragment()
+    var activeFragment:Fragment = fragment1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigation)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        supportFragmentManager.beginTransaction().add(R.id.main_container, fragment2).hide(fragment2).commit()
+        supportFragmentManager.beginTransaction().add(R.id.main_container, fragment3).hide(fragment3).commit()
+        supportFragmentManager.beginTransaction().add(R.id.main_container, fragment4).hide(fragment4).commit()
+        supportFragmentManager.beginTransaction().add(R.id.main_container, fragment5).hide(fragment5).commit()
+        supportFragmentManager.beginTransaction().add(R.id.main_container, fragment1).commit()
+
+        navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.transitionButton -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(fragment1).commit()
+                    activeFragment = fragment1
+                }
+
+                R.id.conditionButton -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(fragment2).commit()
+                    activeFragment = fragment2
+                }
+                R.id.calendarButton -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(fragment3).commit()
+                    activeFragment = fragment3
+                }
+                R.id.mailtrackerButton -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(fragment4).commit()
+                    activeFragment = fragment4
+                }
+                R.id.diettrackerButton -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(fragment5).commit()
+                    activeFragment = fragment5
+                }
+
+            }
+            true
+
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
 
 }
