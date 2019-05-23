e2c059b4546ef923fd68507ec1b5f81535ba373b
Callum Roscoe <Callumroscoe@hotmail.co.uk>
2019-05-23 15:13:26 +0100

    Implement some view designs

--- app/src/main/java/chester/ac/uk/nextgenappandroid/ConditionAboutEdit.kt
+++ app/src/main/java/chester/ac/uk/nextgenappandroid/ConditionAboutEdit.kt
@@ -2,6 +2,11 @@ package chester.ac.uk.nextgenappandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class ConditionAboutEdit : AppCompatActivity() {
 
@@ -9,4 +14,14 @@ class ConditionAboutEdit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_condition_about_edit)
     }
+
+
+    val genderSpinner = findViewById<Spinner>(R.id.genderSpinner)
+
+
+
 }
+
+
+
+
