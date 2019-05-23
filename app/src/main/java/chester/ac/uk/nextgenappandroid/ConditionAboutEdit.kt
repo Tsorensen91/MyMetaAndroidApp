1303fbec73ce4a627051b6039ece58ce9323ace2
Callum Roscoe <Callumroscoe@hotmail.co.uk>
2019-05-23 14:15:24 +0100

    Implement some view designs

---
+++ app/src/main/java/chester/ac/uk/nextgenappandroid/ConditionAboutEdit.kt
@@ -0,0 +1,12 @@
+package chester.ac.uk.nextgenappandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConditionAboutEdit : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_condition_about_edit)
+    }
+}
