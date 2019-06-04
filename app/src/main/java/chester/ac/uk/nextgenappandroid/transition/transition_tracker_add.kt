4b76b1fab5687cc1931eb6cf86965ef008dd71d2
Callum Roscoe <Callumroscoe@hotmail.co.uk>
2019-06-04 12:00:58 +0100

    Added transition tracker add page.
    Complete transition tracker add page layout.

---
+++ app/src/main/java/chester/ac/uk/nextgenappandroid/transition/transition_tracker_add.kt
@@ -0,0 +1,21 @@
+package chester.ac.uk.nextgenappandroid.transition
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import chester.ac.uk.nextgenappandroid.R
+
+
+class transition_tracker_add : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transition_tracker_add, container, false)
+    }
+
+
+}
